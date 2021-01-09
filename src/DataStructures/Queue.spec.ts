282a62a00176dbe2b36eacc256737a34d0338d84
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-01-09 15:53:44 -0500

    Added queue tests and documentation

--- src/DataStructures/Queue.spec.ts
+++ src/DataStructures/Queue.spec.ts
@@ -1,6 +1,7 @@
-/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/valid-expect */ 
 import { expect } from 'chai';
 import 'mocha';
+import { isTemplateSpan } from 'typescript';
 
 import Queue from './Queue';
 
@@ -8,8 +9,74 @@ import Queue from './Queue';
  * Test Suite for Queue class
  */
 describe('Queue', () => {
-  it('Construct Empty Queue', () => {
-    const result = new Queue<number>().toString();
-    expect(result).to.equal('');
+  it('Construct Empty', () => {
+    const queue = new Queue<number>();
+    expect(queue.toString()).to.equal('');
+    expect(queue.getLength()).to.equal(0);
+  });
+
+  it('Construct 1 Element', () => {
+    const queue = new Queue<boolean>([true]);
+    expect(queue.toString()).to.equal('>true');
+    expect(queue.getLength()).to.equal(1);
+  });
+
+  it('Construct 5 Element', () => {
+    const queue = new Queue<number>([1, 2, 3, 4, 4]);
+    expect(queue.toString()).to.equal('>1, 2, 3, 4, 4');
+    expect(queue.getLength()).to.equal(5);
+  });
+
+  it('Add to empty', () => {
+    const queue = new Queue<string>();
+    queue.enqueue('hi');
+    expect(queue.toString()).to.equal('>hi');
+    expect(queue.getLength()).to.equal(1);
+  });
+
+  it('Add to 1', () => {
+    const queue = new Queue<string>(['bye']);
+    queue.enqueue('hi');
+    expect(queue.toString()).to.equal('>bye, hi');
+    expect(queue.getLength()).to.equal(2);
+  });
+
+  it('Add to 3', () => {
+    const queue = new Queue<string>(['bye', 'hello', 'goodbye']);
+    queue.enqueue('hi');
+    expect(queue.toString()).to.equal('>bye, hello, goodbye, hi');
+    expect(queue.getLength()).to.equal(4);
+  });
+
+  it('Remove from empty', () => {
+    const queue = new Queue<boolean>();
+    const result = queue.dequeue();
+    expect(result).to.equal(undefined);
+    expect(queue.toString()).to.equal('');
+    expect(queue.getLength()).to.equal(0);
+  });
+
+  it('Remove from 1', () => {
+    const queue = new Queue<boolean>([true]);
+    const result = queue.dequeue();
+    expect(result).to.equal(true);
+    expect(queue.toString()).to.equal('');
+    expect(queue.getLength()).to.equal(0);
+  });
+
+  it('Remove from 5', () => {
+    const queue = new Queue<number>([7, 5, 3, 1, 1]);
+    const result = queue.dequeue();
+    expect(result).to.equal(7);
+    expect(queue.toString()).to.equal('>5, 3, 1, 1');
+    expect(queue.getLength()).to.equal(4);
+  });
+
+  it('Peek', () => {
+    const queue = new Queue<number>([3, 2, 1]);
+    const result = queue.peek();
+    expect(result).to.equal(3);
+    expect(queue.toString()).to.equal('>3, 2, 1');
+    expect(queue.getLength()).to.equal(3);
   });
 });
--- src/DataStructures/Queue.ts
+++ src/DataStructures/Queue.ts
@@ -1,28 +1,53 @@
-import { stringify } from 'querystring';
 import LinkedList from './LinkedList';
+
+/**
+ * A Generic Queue Class
+ */
 export default class Queue<T> {
   private data: LinkedList<T>;
 
+  /**
+   * Constructs a queue 
+   * @param items The items to initialize with
+   */
   constructor(items: T[] | undefined = undefined) {
     this.data = new LinkedList<T>(items);
   }
 
+  /**
+   * Gets the length of the queue
+   */
   getLength(): number {
     return this.data.getLength();
   }
 
+  /**
+   * Adds an item to the end of the queue
+   * @param data The data to add
+   */
   enqueue(data: T): void {
     this.data.insertAtTail(data);
   }
 
+  /**
+   * Removes and returns the next item from the queue
+   */
   dequeue(): T | undefined {
-    return this.data.removeAtHead();
+    return this.data.removeAt(0);
   }
 
+  /**
+   * Returns the next item in the queue without removing it
+   */
   peek(): T | undefined {
     return this.data.valueAt(0);
   }
 
+  /**
+   * Converts the queue into a string of the form
+   *    '>e1, e1,..., en'
+   * for an n element queue in which e1 is the next element
+   */
   toString(): string {
     if (this.getLength() === 0) {
       return '';
