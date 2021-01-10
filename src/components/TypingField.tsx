bf6840ab1eb90cdac48061564bc4d4e2eae019f8
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-01-10 13:59:24 -0500

    Centered starting elements and removed extra comps

--- src/components/TypingField.tsx
+++ src/components/TypingField.tsx
@@ -1,21 +1,23 @@
-import './Starter';
-import Starter from './Starter';
-
 import './TypingField.css';
+import { Lang } from '../English'
 
-interface Props {
+interface TypingFieldProps {
   button: boolean,
   seconds: number,
   onKeyDown: any,
   clickHandler: (event: React.MouseEvent) => void,
 }
 
-export default function TypingField(props: Props) {
+export default function TypingField(props: TypingFieldProps) {
   if (props.button) {
-    return <Starter clickHandler={props.clickHandler} button={props.button} seconds={props.seconds} />;
+    return ( 
+      <div className='starter'> 
+        <button className='startButton' onClick={props.clickHandler}>{Lang.Start}</button>
+      </div>
+    );
   }
   else if (props.seconds >= 0) {
-    return <div>{props.seconds}</div>;
+    return <div className="starter"><p className='countdown'>{props.seconds}</p></div>;
   }
   else {
     return <textarea onKeyDown={props.onKeyDown} className='InputField'></textarea>;
